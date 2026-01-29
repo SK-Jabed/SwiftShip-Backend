dd9ee5b6b038da7fbcc560e07e67d0609f632f31
Sheikh Jabed <jabedbd2295@gmail.com>
2026-01-29 22:44:48 +0600

    New Parcel History Related Activities Updated

--- src/app/modules/OTP/otp.controllers.ts
+++ src/app/modules/OTP/otp.controllers.ts
@@ -4,30 +4,31 @@ import { sendResponse } from "../../utils/sendResponse";
 import { OTPService } from "./otp.service";
 
 const sendOTP = catchAsync(async (req: Request, res: Response) => {
-    const { email, name } = req.body
-    await OTPService.sendOTP(email, name)
+  const { email, name } = req.body;
+  await OTPService.sendOTP(email, name);
 
-    sendResponse(res, {
-        statusCode: 201,
-        data: null,
-        message: "OTP sent Done",
-        success: true
-    })
+  sendResponse(res, {
+    statusCode: 201,
+    data: null,
+    message: "OTP sent Done",
+    success: true,
+  });
 });
+
 const verifyOTP = catchAsync(async (req: Request, res: Response) => {
-    const { email, otp } = req.body
+  const { email, otp } = req.body;
 
-    await OTPService.verifyOTP(email, otp)
+  await OTPService.verifyOTP(email, otp);
 
-    sendResponse(res, {
-        statusCode: 201,
-        data: null,
-        message: "Verified Done",
-        success: true
-    })
+  sendResponse(res, {
+    statusCode: 201,
+    data: null,
+    message: "Verified Done",
+    success: true,
+  });
 });
 
 export const OTPController = {
-    sendOTP,
-    verifyOTP
+  sendOTP,
+  verifyOTP,
 };
