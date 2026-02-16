42471564dcfb57cfa59bc341f9f03e42f9134221
Sheikh Jabed <jabedbd2295@gmail.com>
2026-02-16 22:19:21 +0600

    Global Error Handler and Other Middleware Updated

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -5,26 +5,24 @@ import { envVars } from "./env";
 import AppError from "../errorHelpers/AppError";
 
 cloudinary.config({
-    cloud_name: envVars.CLOUDINARY.CLOUDINARY_CLOUD_NAME,
-    api_key: envVars.CLOUDINARY.CLOUDINARY_API_KEY,
-    api_secret: envVars.CLOUDINARY.CLOUDINARY_API_SECRET
-})
+  cloud_name: envVars.CLOUDINARY.CLOUDINARY_CLOUD_NAME,
+  api_key: envVars.CLOUDINARY.CLOUDINARY_API_KEY,
+  api_secret: envVars.CLOUDINARY.CLOUDINARY_API_SECRET,
+});
 
 export const deleteImageFromCloudinary = async (url: string) => {
-    try {
-        const regex = /\/v\d+\/(.*?)\.(jpg|png|jpeg|gif|webp)/i;
-        const match = url.match(regex)
-        if (match && match[1]) {
-            const publicId = match[1]
+  try {
+    const regex = /\/v\d+\/(.*?)\.(jpg|png|jpeg|gif|webp)/i;
+    const match = url.match(regex);
+    if (match && match[1]) {
+      const publicId = match[1];
 
-            await cloudinary.uploader.destroy(publicId)
-            console.log(publicId)
-        }
-    } catch (error: any) {
-        throw new AppError(404, "cloudinary image failed to delete", error.message)
+      await cloudinary.uploader.destroy(publicId);
+      console.log(publicId);
     }
-}
+  } catch (error: any) {
+    throw new AppError(404, "cloudinary image failed to delete", error.message);
+  }
+};
 
-
-export const cloudinaryUpload = cloudinary
-
+export const cloudinaryUpload = cloudinary;
